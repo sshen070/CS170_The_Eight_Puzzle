b90e826b7223bd5318d917e55cbe996b0e80b8f5
sshen070 <SujithShen@gmail.com>
2026-02-10 18:40:12 -0800

    findNeigborsStates() & deepCopy() successfully implemented

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -34,8 +34,6 @@ class puzzleState:
         self.state = currPuzzle 
         self.prev = None
 
-    
-    
 def randomPuzzle():
     return default_states[random.randint(0, len(default_states) - 1)]
 
@@ -51,34 +49,105 @@ def printPuzzle(self):
     print(puzzle_str)
 
 
+def findOpenSpot(self):
+    for i in range (len(self.state)):
+        for j in range (len(self.state[i])):
+            if (self.state[i][j] == 0):
+                return i, j
+
+
+# Find all possible state transistions
+def findNeighborStates(puzzle):
+    neighbors = []
+
+    # Contains x & y coords
+    x_coord, y_coord = findOpenSpot(puzzle)
+    # print (x_coord, y_coord)
+
+    # Compare with x & y coords to see if transition possible
+    transitions_values = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+    for transition in transitions_values:
+
+        # Potential transition in x-axis
+        if (transition[0] != 0):
+            
+            # If empty spot '0' is not in the leftmost or rightmost column
+            # Check if state is within bounds
+            if x_coord + transition[0] >= 0 and x_coord + transition[0] < len(puzzle.state):
+                
+                # Copy over puzzle state as template and swap values
+                new_state = puzzleState(deepCopy(puzzle))
+                
+                # Swap
+                swap_coord = new_state.state[x_coord + transition[0]][y_coord ]
+                new_state.state[x_coord + transition[0]][y_coord] = new_state.state[x_coord][y_coord]
+                new_state.state[x_coord][y_coord] = swap_coord
+
+                neighbors.append(new_state)
+
+        if (transition[1] != 0):
+            # Same idea for y. If empty spot '0' is not in the first or last (3rd) row
+            if y_coord + transition[1] >= 0 and y_coord + transition[1] < len(puzzle.state[0]):
+                
+                # Copy over puzzle state as template
+                new_state = puzzleState(deepCopy(puzzle))
+
+                # Swap
+                swap_coord = new_state.state[x_coord][y_coord + transition[1]]
+                new_state.state[x_coord][y_coord + transition[1]] = new_state.state[x_coord][y_coord]
+                new_state.state[x_coord][y_coord] = swap_coord
+
+                neighbors.append(new_state)
+
+    return neighbors
+
+# Deep copy required to create new puzzle state
+def deepCopy(self):
+    puzzle_copy = [[0 for _ in range(len(self.state))] for _ in range(len(self.state[0]))]
+
+    for i in range (len(self.state)):
+        for j in range (len(self.state[i])):
+            puzzle_copy[i][j] = self.state[i][j]
+
+    return puzzle_copy
+
+
+
+
 def main():
     puzzle_mode = input("Welcome to an 8-Puzzle Solver. Type '1' to use a default puzzle, "
     "or '2' to create your own."+ '\n')
 
     if puzzle_mode == "1":
-        puzzle1 = puzzleState(randomPuzzle())
-        printPuzzle(puzzle1)
+        puzzle = puzzleState(randomPuzzle())
+        printPuzzle(puzzle)
+        neighbors = findNeighborStates(puzzle)
 
-    if puzzle_mode == "2":
-        print("Enter your puzzle, using a zero to represent the blank. " +
-        "Please only enter valid 8-puzzles. Enter the puzzle demilimiting " 
-        + "the numbers with a space. Type RETURN only when finished." + '\n')
+        for i in range (len(neighbors)):
+            print(f'Neighbor {i + 1}:') 
+            printPuzzle(neighbors[i])
 
-        puzzle_row_one = input("Enter the first row: ")
-        puzzle_row_two = input("Enter the second row: ")
-        puzzle_row_three = input("Enter the third row: ")
+    # if puzzle_mode == "2":
+    #     print("Enter your puzzle, using a zero to represent the blank. " +
+    #     "Please only enter valid 8-puzzles. Enter the puzzle demilimiting " 
+    #     + "the numbers with a space. Type RETURN only when finished." + '\n')
 
-        puzzle_row_one = puzzle_row_one.split()
-        puzzle_row_two = puzzle_row_two.split()
-        puzzle_row_three = puzzle_row_three.split()
+    #     puzzle_row_one = input("Enter the first row: ")
+    #     puzzle_row_two = input("Enter the second row: ")
+    #     puzzle_row_three = input("Enter the third row: ")
 
-        # Matrix positions must be ints
-        for i in range (0, 3):
-            puzzle_row_one[i] = int(puzzle_row_one[i])
-            puzzle_row_two[i] = int(puzzle_row_two[i])
-            puzzle_row_three[i] = int(puzzle_row_three[i])
+    #     puzzle_row_one = puzzle_row_one.split()
+    #     puzzle_row_two = puzzle_row_two.split()
+    #     puzzle_row_three = puzzle_row_three.split()
 
-        puzzle1 = puzzleState([[puzzle_row_one][puzzle_row_two][puzzle_row_three]])
+    #     # Matrix positions must be ints
+    #     for i in range (0, 3):
+    #         puzzle_row_one[i] = int(puzzle_row_one[i])
+    #         puzzle_row_two[i] = int(puzzle_row_two[i])
+    #         puzzle_row_three[i] = int(puzzle_row_three[i])
+
+    #     puzzle1 = puzzleState([[puzzle_row_one][puzzle_row_two][puzzle_row_three]])
 
 if __name__ == '__main__':
     main()
